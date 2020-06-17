37cb0c949e3af66e76665da8238c80f939546a13
Renata <is.ren.id@gmail.com>
2020-06-17 14:31:05 +0300

    Дом задание лекция 4 не получается, как нужно сделать лучше скажите пожалуйста.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+/* eslint-disable semi */
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-undef */
 'use strict';
 var picturesTemplate = document.querySelector('#picture')
 .content
@@ -13,7 +16,7 @@ var commentsGeneration = function () {
   var commentsEmpty = [];
   for (var i = 0; i <= Math.floor(Math.random() * 7); i++) {
     var object = {};
-    object.avatar = '"img/avatar-' + (Math.floor(Math.random() * (nameAuthorsRandom.length - 1)) + 1) + '.svg"';
+    object.avatar = 'img/avatar-' + (Math.floor(Math.random() * (nameAuthorsRandom.length - 1)) + 1) + '.svg';
     if (Math.floor(Math.random() * 2) === 1) {
       var message1 = messageRandom[Math.floor(Math.random() * (messageRandom.length - 1)) + 1];
       var message2 = messageRandom[Math.floor(Math.random() * (messageRandom.length - 1)) + 1];
@@ -68,7 +71,7 @@ var similarListElement = bigWindow.querySelector('.social__comments');
 var socialCommentCount = bigWindow.querySelector('.social__comment-count');
 var commentsLoader = bigWindow.querySelector('.comments-loader');
 
-bigWindow.classList.remove('hidden');
+//bigWindow.classList.remove('hidden');
 
 bigWindow.querySelector('.big-picture__img img').src = arrow[1].url;
 bigWindow.querySelector('.likes-count').textContent = arrow[1].likes;
@@ -91,4 +94,94 @@ for (var commentNumber = 0; commentNumber < arrow[1].comments.length; commentNum
 similarListElement.appendChild(fragmentCommentBigPhoto);
 socialCommentCount.classList.add('hidden');
 commentsLoader.classList.add('hidden');
-document.querySelector('body').classList.add('modal-open');
+//document.querySelector('body').classList.add('modal-open');
+
+
+// дом задание лекция 4
+var uploadField = picturesWindow.querySelector('#upload-file');// поле выбора файла
+var uploadCansel = picturesWindow.querySelector('#upload-cancel');
+var editForm = picturesWindow.querySelector('.img-upload__overlay');
+
+var effectLevelSlider = picturesWindow.querySelector('.effect-level__pin');// ползунок
+var effectLevelLine = picturesWindow.querySelector('.effect-level__depth');
+var effectLevelInput = picturesWindow.querySelector('.effect-level__value');
+var fieldsetFilterList = picturesWindow.querySelector('.img-upload__effects');
+
+var fieldsetHashtag = picturesWindow.querySelector('.img-upload__text');
+var textHashtag = fieldsetHashtag.querySelector('.text__hashtags');
+var uploadSubmit = picturesWindow.querySelector('.img-upload__submit');
+var reg = /^#[a-zA-Z0-9_]{1,20}$/ig;
+var NULL_POSITION = 488;
+var NULL_WIDTH = 20;
+var onPopupEscPress = function (evt) {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    closePopup();
+  }
+};
+var openPopup = function () {
+  document.querySelector('body').classList.add('modal-open');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function () {
+  document.querySelector('body').classList.remove('modal-open');
+  document.removeEventListener('keydown', onPopupEscPress);
+  uploadField.value = ''; // сброс значения поля выбора
+};
+
+uploadField.addEventListener('change', function () {
+  openPopup();
+  editForm.classList.remove('hidden');
+});
+
+uploadCansel.addEventListener('click', function () {
+  closePopup();
+});
+
+// координата по х отпускания мыши
+effectLevelSlider.addEventListener('mouseup', function (e) {
+  var newWidth = e.clientX * NULL_WIDTH / NULL_POSITION;
+  effectLevelSlider.style.left = newWidth + '%';
+  effectLevelLine.style.width = newWidth + '%';
+  effectLevelInput.value = newWidth;
+});
+// обработчик на радиокнопки (при изменении состояния возращает начальное положение )
+fieldsetFilterList.addEventListener('change', function () {
+  effectLevelLine.style.width = NULL_WIDTH + '%';
+  effectLevelSlider.style.left = NULL_WIDTH + '%';
+  effectLevelInput.value = NULL_WIDTH;
+});
+
+
+uploadSubmit.addEventListener('submit', function (evt) {
+  textHashtag.addEventListener('input', function () {
+    var hashtagLine = textHashtag.value;
+    console.log(hashtagLine);
+    var hashtagArr = hashtagLine.toLowerCase().split(' ');
+
+    for (var hash = 0; hash < hashtagArr.length; hash++) {
+
+      if (reg.test(hashtagArr[hash])) {
+
+        var hashExamle = hashtagArr[hash];
+        for (var k = hash + 1; k < hashtagArr.length; k++) {
+          if (hashExamle === hashtagArr[k]) {
+            textHashtag.setCustomValidity('Нельзя вводить один и тот же хештег!');
+            evt.preventDefault();
+          }
+        }
+      } else {
+        textHashtag.setCustomValidity('Некорректно введен хештег!');
+        evt.preventDefault();
+      }
+    }
+    if (hashtagArr.length > 5) {
+      textHashtag.setCustomValidity('Нельзя использовать более 5ти хештегов!');
+      evt.preventDefault();
+    }
+  });
+
+});
+
+
