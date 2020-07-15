b27473ed90a3c651c8fc1ab7f80a4c0ca5ccc8f1
Рената <is.ren.id@gmail.com>
2020-07-15 22:13:00 +0300

    сделала задание

--- js/picture.js
+++ js/picture.js
@@ -12,27 +12,14 @@
   var randomButton = filterButtons.querySelector('#filter-random');
   var ratingCommentButton = filterButtons.querySelector('#filter-discussed');
 
-  var getArr = function (arr) {
+  function getArr(arr) {
     var photoElement = picturesTemplate.cloneNode(true);
     photoElement.querySelector('.picture__img').src = arr.url;
     photoElement.querySelector('.picture__likes').textContent = arr.likes;
     photoElement.querySelector('.picture__comments').textContent = arr.comments.length;
     return photoElement;
-  };
+  }
 
-  window.picture = function (data) {
-    window.data = data;
-    filterButtons.classList.remove('img-filters--inactive');
-    defaultRendering(window.data);
-  };
-  function removeClassButton(button) {
-    if (button.classList.contains('img-filters__button--active')) {
-      button.classList.remove('img-filters__button--active');
-    }
-  }
-  function toggleClassButton(button) {
-    button.classList.toggle('img-filters__button--active');
-  }
   function defaultRendering(data) {
     var fragment = document.createDocumentFragment();
     for (var i = 0; i < data.length; i++) {
@@ -41,77 +28,105 @@
     picturesWindow.appendChild(fragment);
   }
 
+  window.picture = function (data) {
+    window.data = data;
+    filterButtons.classList.remove('img-filters--inactive');
+    defaultRendering(window.data);
+  };
+
+
+  function removeClassButton(button) {
+    if (button.classList.contains('img-filters__button--active')) {
+      button.classList.remove('img-filters__button--active');
+    }
+  }
+  function toggleClassButton(button) {
+    button.classList.toggle('img-filters__button--active');
+  }
+
   function getRank(arr) {
     return arr.comments.length;
   }
+
   function ratingCommentFilter() {
-    var picturesElements = picturesWindow.querySelectorAll('.picture');
-    for (var i = 0; i < picturesElements.length; i++) {
-      picturesWindow.removeChild(picturesElements[i]);
-    }
     var popularElements = window.data.slice().sort(function (left, right) {
       var rankDiff = getRank(right) - getRank(left);
       return rankDiff;
     });
-    defaultRendering(popularElements.slice(0, 10));
-    removeClassButton(randomButton);
-    removeClassButton(defaultButton);
-    toggleClassButton(ratingCommentButton);
-  }
-
-  function defaultFilter() {
-    var picturesElements = picturesWindow.querySelectorAll('.picture');
-    for (var i = 0; i < picturesElements.length; i++) {
-      picturesWindow.removeChild(picturesElements[i]);
-    }
-    defaultRendering(window.data);
-    removeClassButton(randomButton);
-    removeClassButton(ratingCommentButton);
-    toggleClassButton(defaultButton);
+    return popularElements.slice(0, 10);
   }
 
   function randomFilter() {
-    var picturesElements = picturesWindow.querySelectorAll('.picture');
-    if (!randomButton.classList.contains('img-filters__button--active')) {
-      var arr = []; // записываем в этот массив рандомные числа
-      var max = 10; // максимальная длина массива
-      var rundomnumber; // случайное число
+    var arr = []; // записываем в этот массив рандомные числа
+    var max = 10; // максимальная длина массива
+    var rundomnumber; // случайное число
 
-      while (arr.length < max) {
-        rundomnumber = Math.floor(Math.random() * window.data.length); // создадим случайное число
-        if (arr.indexOf(rundomnumber) === -1) { // проверим есть оно  у нас или нет
-          arr.push(rundomnumber); // записываем в массив т.к нету
-        }
+    while (arr.length < max) {
+      rundomnumber = Math.floor(Math.random() * window.data.length); // создадим случайное число
+      if (arr.indexOf(rundomnumber) === -1) { // проверим есть оно  у нас или нет
+        arr.push(rundomnumber); // записываем в массив т.к нету
       }
-      for (var i = 0; i < arr.length; i++) {
-        arr[i] = window.data[arr[i]];
+    }
+    for (var i = 0; i < arr.length; i++) {
+      arr[i] = window.data[arr[i]];
+    }
+    // код ниже позволяет частично перерендаривать после клика
+    // if (defaultButton.classList.contains('img-filters__button--active')) {
+    //   for (i = 0; i < window.data.length; i++) {
+    //     if (!arr.includes(window.data[i])) {
+    //       picturesWindow.removeChild(picturesElements[i]);
+    //     }
+    //   }
+    // } else {
+    //   for (i = 0; i < picturesElements.length; i++) {
+    //     picturesWindow.removeChild(picturesElements[i]);
+    //   }
+    //   defaultRendering(arr);
+    // }
+    return arr;
+  }
+
+  function filterFunction(butt1, butt2, buttoncurrent) {
+    if (!buttoncurrent.classList.contains('img-filters__button--active')) {
+      var picturesElements = picturesWindow.querySelectorAll('.picture');
+      for (var i = 0; i < picturesElements.length; i++) {
+        picturesWindow.removeChild(picturesElements[i]);
       }
-      if (defaultButton.classList.contains('img-filters__button--active')) {
-        for (i = 0; i < window.data.length; i++) {
-          if (!arr.includes(window.data[i])) {
-            picturesWindow.removeChild(picturesElements[i]);
-          }
-        }
-      } else {
-        for (i = 0; i < picturesElements.length; i++) {
-          picturesWindow.removeChild(picturesElements[i]);
-        }
-        defaultRendering(arr);
+      var arrow;
+      switch (buttoncurrent) {
+        case randomButton:
+          arrow = randomFilter();
+          break;
+        case ratingCommentButton:
+          arrow = ratingCommentFilter();
+          break;
+        case defaultButton:
+          arrow = window.data;
+          break;
+        default:
+          break;
       }
-      removeClassButton(defaultButton);
-      removeClassButton(ratingCommentButton);
-      toggleClassButton(randomButton);
+      defaultRendering(arrow);
+      removeClassButton(butt1);
+      removeClassButton(butt2);
+      toggleClassButton(buttoncurrent);
     }
   }
-  var onRandomClick = window.debounce(randomFilter);
-  var onRatingCommentClick = window.debounce(ratingCommentFilter);
-  var onDefaultClick = window.debounce(defaultFilter);
+  function button1() {
+    filterFunction(randomButton, ratingCommentButton, defaultButton);
+  }
+  function button2() {
+    filterFunction(defaultButton, ratingCommentButton, randomButton);
+  }
+  function button3() {
+    filterFunction(defaultButton, randomButton, ratingCommentButton);
+  }
+  var onRandomClick = window.debounce(button2);
+  var onRatingCommentClick = window.debounce(button3);
+  var onDefaultClick = window.debounce(button1);
   randomButton.addEventListener('click', onRandomClick);
   defaultButton.addEventListener('click', onDefaultClick);
   ratingCommentButton.addEventListener('click', onRatingCommentClick);
-  // var fragment = document.createDocumentFragment();
-  // for (var i = 1; i < 26; i++) {
-  //   fragment.appendChild(getPhotos(i));
-  // }
-  // picturesWindow.appendChild(fragment);
 }());
+
+
